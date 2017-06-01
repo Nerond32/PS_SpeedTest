cf958941af344fd0e185ea743f01d0b6e579af29
Nerond32 <nerond32@gmail.com>
2017-06-01 10:51:47 +0200

    SpeedTest init

---
+++ SpeedTester/SpeedTester/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Threading;
+using System.Windows;
+
+namespace SpeedTester
+{
+    public partial class MainWindow : Window
+    {
+        private bool running = false;
+        Thread tcpServerThread, tcpClientThread;
+        ServerTCP tcpS;
+        ClientTCP tcpC;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += MainWindow.OnWindowClosing;
+        }
+
+        private static void OnWindowClosing(object sender, CancelEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        void ChangeVisibility(object sender, EventArgs e)
+        {
+            clientGrid.Visibility = Visibility.Hidden;
+            serverGrid.Visibility = Visibility.Hidden;
+            if(serverOrClientCB.Text == "Server")
+            {
+                serverGrid.Visibility = Visibility.Visible;
+            }
+            if(serverOrClientCB.Text == "Client")
+            {
+                clientGrid.Visibility = Visibility.Visible;
+            }
+        }
+
+        void ChangeServerStatus(object sender, EventArgs e)
+        {
+            if(running == true)
+            {
+                serverStatusButton.Content = "Start server"; 
+                running = false;
+                tcpS.RequestStop();
+                //tcpThread.Abort();
+                return;
+            }
+            IPAddress ipAddress;
+            int port;
+            try
+            {
+                ipAddress = IPAddress.Parse(ipTextBox.Text);
+                port = Int32.Parse(portTextBox.Text);
+                running = true;
+                serverStatusButton.Content = "Stop server";
+                tcpS = new ServerTCP(ipAddress, port);
+                tcpServerThread = new Thread(tcpS.Run);
+                tcpServerThread.Start();
+            }
+            catch
+            {
+                running = false;
+                MessageBox.Show("Invalid ip address or port entered!", "Input error");
+            }
+        }
+
+        private void ClientConnect(object sender, RoutedEventArgs e)
+        {
+            IPAddress ipAddress;
+            int port;
+            if (running == true)
+            {
+                clientConnectButton.Content = "Connect";
+                running = false;
+                return;
+            }
+            try
+            {
+                ipAddress = IPAddress.Parse(ipTextBox.Text);
+                port = Int32.Parse(portTextBox.Text);
+                running = true;
+                clientConnectButton.Content = "Disconnect";
+                tcpC = new ClientTCP(ipAddress, port);
+                tcpClientThread = new Thread(tcpC.Run);
+                tcpClientThread.Start();
+            }
+            catch
+            {
+                running = false;
+                MessageBox.Show("Invalid ip address or port entered!", "Input error");
+            }
+        }
+    }
+}
---
+++ SpeedTester/SpeedTester/ServerTCP.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedTester
+{
+    class ServerTCP
+    {
+        bool isRunning = false;
+        IPAddress ipAddress;
+        int port;
+        Socket clientSocket;
+        public ServerTCP(IPAddress ipAddress, int port)
+        {
+            this.ipAddress = ipAddress;
+            this.port = port;
+        }
+        public Socket InitServer()
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+            s.Bind(localEndPoint);
+            return s;
+        }
+        public void Run()
+        {
+            isRunning = true;
+            Socket serverSocket = InitServer();
+            Console.WriteLine("Starting server at: " + serverSocket.LocalEndPoint);
+            serverSocket.Listen(10);
+            while (isRunning)
+            {
+                Console.WriteLine("Waiting for new connection.");
+                clientSocket = serverSocket.Accept();
+                Console.WriteLine("Connection accepted from " + clientSocket.RemoteEndPoint.ToString());
+                byte[] fromServer = new byte[clientSocket.ReceiveBufferSize];
+                int length = clientSocket.Receive(fromServer);
+                String ts = Encoding.UTF8.GetString(fromServer).Substring(0, length);
+                Console.WriteLine("Message received from client: " + ts);
+                clientSocket.Send(Encoding.UTF8.GetBytes(ts));
+                clientSocket.Close();
+            }
+            serverSocket.Close();
+            Console.ReadLine();
+        }
+        public void RequestStop()
+        {
+            isRunning = false;
+            //clientSocket.Shutdown(SocketShutdown.Both);
+            //clientSocket.Close();
+            //clientSocket.LingerState = new LingerOption(true, 0);
+            //clientSocket.Close();
+        }
+    }
+}
